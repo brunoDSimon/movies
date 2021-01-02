81ce8982625f221f0efc68cf113a234a3084ab6d
Bruno <brunosimon999@gmail.com>
2021-01-02 09:31:35 -0300

    heroku

---
+++ server.js
@@ -0,0 +1,6 @@
+const app = express();
+app.use(express.static('./dist/projectAngular'));
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/projectAngular/'}),
+);
+app.listen(process.env.PORT || 8080);
