c96de4c081d2e56aa4a144d924b0fcc428510ff9
Bruno <brunosimon999@gmail.com>
2021-01-02 10:17:59 -0300

    import

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const express = require('express');
 const app = express();
+
 app.use(express.static('./dist/projectAngular'));
+
 app.get('/*', (req, res) =>
   res.sendFile('index.html', {root: 'dist/projectAngular/'}),
 );
+
 app.listen(process.env.PORT || 8080);
